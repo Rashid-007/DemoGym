acf497670ab1c3e105d43c429cb463d71038adff
Tyler Sadler <tssadler88@gmail.com>
2017-01-14 22:47:43 -0600

    rename spring boot app add dependancies to pom

--- src/main/java/com/tylersadler/DemoGymApplication.java
+++ src/main/java/com/tylersadler/DemoGymApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemogymApplication {
+public class DemoGymApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemogymApplication.class, args);
+		SpringApplication.run(DemoGymApplication.class, args);
 	}
 }
